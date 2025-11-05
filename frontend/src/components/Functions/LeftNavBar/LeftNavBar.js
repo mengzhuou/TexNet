ded751895bbdda799c03be741f40dbf6c46bfb55
0812Sean <dachenxuan@gmail.com>
2025-11-04 23:23:48 -0600

    2

--- frontend/src/components/Functions/LeftNavBar/LeftNavBar.js
+++ frontend/src/components/Functions/LeftNavBar/LeftNavBar.js
@@ -3,14 +3,8 @@ import { withFuncProps } from "../../withFuncProps";
 import NavButton from "../../Button/NavButton/NavButton";
 import styles from "./LeftNavBar.module.scss";
 import { 
-  FiHome, 
-  FiUser, 
-  FiMessageSquare, 
-  FiMail, 
-  FiUsers, 
-  FiSettings, 
-  FiPhone, 
-  FiSend 
+  FiHome, FiUser, FiMessageSquare, FiMail, FiUsers, 
+  FiSettings, FiPhone, FiSend 
 } from 'react-icons/fi';
 
 class LeftNavBar extends Component {
@@ -18,19 +12,22 @@ class LeftNavBar extends Component {
     if (window.location.pathname === "/") {
       return <div className={styles.sideNavBar}></div>;
     }
+
+    const currentPath = window.location.pathname;
+
     return (
       <div className={styles.sideNavBar}>
         <ul className={styles.sideNavList}>
           <li className={styles.sideNavItem}>
-            <NavButton className={styles.sideNavButton} icon={<FiHome />} path="/mainPage" text="Home" />
-            <NavButton className={styles.sideNavButton} icon={<FiUser />} path="/profile" text="Profile" />
-            <NavButton className={styles.sideNavButton} icon={<FiMessageSquare />} path="/chatHistory" text="Chat History" />
-            <NavButton className={styles.sideNavButton} icon={<FiMail />} path="/smsHistory" text="SMS History" />
-            <NavButton className={styles.sideNavButton} icon={<FiUsers />} path="/contactManagement" text="Contact Management" />
-            <NavButton className={styles.sideNavButton} icon={<FiUsers />} path="/users" text="Users" />
-            <NavButton className={styles.sideNavButton} icon={<FiSettings />} path="/callflow" text="Callflow" />
-            <NavButton className={styles.sideNavButton} icon={<FiSend />} path="/messageEngine" text="Message Engine" />
-            <NavButton className={styles.sideNavButton} icon={<FiPhone />} path="/callMessage" text="Call/Message" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/mainPage" ? styles.active : ""}`} icon={<FiHome />} path="/mainPage" text="Home" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/profile" ? styles.active : ""}`} icon={<FiUser />} path="/profile" text="Profile" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/chatHistory" ? styles.active : ""}`} icon={<FiMessageSquare />} path="/chatHistory" text="Chat History" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/smsHistory" ? styles.active : ""}`} icon={<FiMail />} path="/smsHistory" text="SMS History" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/contactManagement" ? styles.active : ""}`} icon={<FiUsers />} path="/contactManagement" text="Contact Management" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/users" ? styles.active : ""}`} icon={<FiUsers />} path="/users" text="Users" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/callflow" ? styles.active : ""}`} icon={<FiSettings />} path="/callflow" text="Callflow" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/messageEngine" ? styles.active : ""}`} icon={<FiSend />} path="/messageEngine" text="Message Engine" />
+            <NavButton className={`${styles.sideNavButton} ${currentPath === "/callMessage" ? styles.active : ""}`} icon={<FiPhone />} path="/callMessage" text="Call/Message" />
           </li>
         </ul>
       </div>
